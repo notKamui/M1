4c4ca6ba5d10daad722bf7181c6849d18c15daba
notKamui <teillard.jimmy@outlook.fr>
2021-10-01 14:05:11 +0200

    Update

--- DesignPattern/TD1/src/fr/uge/poo/simplegraphics/SimpleGraphics.java
+++ DesignPattern/TD1/src/fr/uge/poo/simplegraphics/SimpleGraphics.java
@@ -22,18 +22,45 @@ import java.util.function.Consumer;
  * thread than the own calling it. This can create data races if the consumer uses non-final values.
  */
 public class SimpleGraphics {
+    private final JComponent area;
+    private final BufferedImage buffer;
     private static final MouseEvent CLOSE_EVENT =
             new MouseEvent(new JButton(), -1, -1, -1, 0, 0, 0, 0, 0, false, 0);
     final LinkedBlockingQueue<MouseEvent> eventBlockingQueue =
             new LinkedBlockingQueue<>();
-    private final JComponent area;
-    private final BufferedImage buffer;
+
+    /**
+     * Clear the drawing area with the color of the brush.
+     */
+    public void clear(Color color) {
+        render(graphics -> {
+            graphics.setColor(color);
+            graphics.fillRect(0, 0, area.getWidth(), area.getHeight());
+        });
+    }
+
+    /**
+     * Ask to render something on the screen.
+     * @param consumer a code that will draw on the screen.
+     */
+    public void render(Consumer<Graphics2D> consumer) {
+        EventQueue.invokeLater(() -> {
+            Graphics2D graphics = buffer.createGraphics();
+            try {
+                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                consumer.accept(graphics);
+                area.repaint();  // repost a paint event to avoid blinking
+            } finally {
+                graphics.dispose();
+            }
+        });
+    }
 
     /**
      * Create a window of size width x height and a title.
      *
-     * @param title  the title of the window.
-     * @param width  the width of the window.
+     * @param title the title of the window.
+     * @param width the width of the window.
      * @param height the height of the window.
      */
     public SimpleGraphics(String title, int width, int height) {
@@ -65,6 +92,7 @@ public class SimpleGraphics {
                 mouseClicked(e);
             }
         }
+        ;
         JComponent() {
             @Override
             protected void paintComponent (Graphics g){
@@ -99,42 +127,15 @@ public class SimpleGraphics {
     }
 
     /**
-     * Clear the drawing area with the color of the brush.
-     */
-    public void clear(Color color) {
-        render(graphics -> {
-            graphics.setColor(color);
-            graphics.fillRect(0, 0, area.getWidth(), area.getHeight());
-        });
-    }
-
-    /**
-     * Ask to render something on the screen.
-     *
-     * @param consumer a code that will draw on the screen.
-     */
-    public void render(Consumer<Graphics2D> consumer) {
-        EventQueue.invokeLater(() -> {
-            Graphics2D graphics = buffer.createGraphics();
-            try {
-                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                consumer.accept(graphics);
-                area.repaint();  // repost a paint event to avoid blinking
-            } finally {
-                graphics.dispose();
-            }
-        });
-    }
-
-    /**
      * Wait for mouse events, the mouseCallback method
      * {@link MouseCallback#mouseClicked(int, int)}
      * will be called for each mouse event until the window
      * is {@link #close() closed}.
      *
      * @param mouseCallback a mouse callback.
+     *
      * @throws IllegalStateException if this method is
-     *                               called by the event dispatch thread.
+     *         called by the event dispatch thread.
      */
     public void waitForMouseEvents(MouseCallback mouseCallback) {
         if (EventQueue.isDispatchThread()) {
@@ -169,17 +170,16 @@ public class SimpleGraphics {
     }
 
     /**
-     * A functional interface of the mouse callback.
-     *
-     * @see SimpleGraphics#waitForMouseEvents(MouseCallback)
+     *  A functional interface of the mouse callback.
+     *  @see SimpleGraphics#waitForMouseEvents(MouseCallback)
      */
     @FunctionalInterface
     public interface MouseCallback {
         /**
          * Called when the mouse is used inside the canvas.
-         *
          * @param x x coordinate of the mouse.
          * @param y y coordinate of the mouse
+         *
          * @see SimpleGraphics#waitForMouseEvents(MouseCallback)
          */
         void mouseClicked(int x, int y);
