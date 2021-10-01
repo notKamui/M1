cd9389756e6cc15c3fd5163a7de8bfd94151b9a2
notKamui <teillard.jimmy@outlook.fr>
2021-10-01 14:06:12 +0200

    Update

--- DesignPattern/TD1/src/fr/uge/poo/simplegraphics/SimpleGraphics.java
+++ DesignPattern/TD1/src/fr/uge/poo/simplegraphics/SimpleGraphics.java
@@ -11,23 +11,89 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 
 /**
- * This class represents a drawing area.
- * <p>
- * The method {@link #clear(Color)} can be used to set a background color.
- * The method {@link #render(Consumer)} can be used to draw something one screen.
- * <p>
- * The mouse events can be retrieved using {@link #waitForMouseEvents(MouseCallback)}.
- * <p>
- * All these methods are thread safe. However, the consumer provided to render is run in a different
- * thread than the own calling it. This can create data races if the consumer uses non-final values.
+ *  This class represents a drawing area.
+ *
+ *  The method {@link #clear(Color)} can be used to set a background color.
+ *  The method {@link #render(Consumer)} can be used to draw something one screen.
+ *
+ *  The mouse events can be retrieved using {@link #waitForMouseEvents(MouseCallback)}.
+ *
+ *  All these methods are thread safe. However, the consumer provided to render is run in a different
+ *  thread than the own calling it. This can create data races if the consumer uses non-final values.
+ *
  */
 public class SimpleGraphics {
     private final JComponent area;
     private final BufferedImage buffer;
-    private static final MouseEvent CLOSE_EVENT =
-            new MouseEvent(new JButton(), -1, -1, -1, 0, 0, 0, 0, 0, false, 0);
     final LinkedBlockingQueue<MouseEvent> eventBlockingQueue =
             new LinkedBlockingQueue<>();
+
+    /**
+     * Create a window of size width x height and a title.
+     *
+     * @param title the title of the window.
+     * @param width the width of the window.
+     * @param height the height of the window.
+     */
+    public SimpleGraphics(String title, int width, int height) {
+        // This should be a factory method and not a constructor
+        // but given that this library can be used
+        // before static factory have been introduced
+        // it's simpler from the user point of view
+        // to create a canvas area using new.
+
+        BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        @SuppressWarnings("serial")
+        JComponent area = new JComponent() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.drawImage(buffer, 0, 0, null);
+            }
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(width, height);
+            }
+        };
+        class MouseManager extends MouseAdapter implements MouseMotionListener {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                try {
+                    eventBlockingQueue.put(event);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                // do nothing
+            }
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                mouseClicked(e);
+            }
+        }
+        MouseManager mouseManager = new MouseManager();
+        area.addMouseListener(mouseManager);
+        area.addMouseMotionListener(mouseManager);
+        try {
+            EventQueue.invokeAndWait(() -> {
+                JFrame frame = new JFrame(title);
+                area.setOpaque(true);
+                frame.setContentPane(area);
+                frame.setResizable(false);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            });
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch(InvocationTargetException e) {
+            throw new IllegalStateException(e.getCause());
+        }
+        this.buffer = buffer;
+        this.area = area;
+    }
 
     /**
      * Clear the drawing area with the color of the brush.
@@ -57,73 +123,19 @@ public class SimpleGraphics {
     }
 
     /**
-     * Create a window of size width x height and a title.
-     *
-     * @param title the title of the window.
-     * @param width the width of the window.
-     * @param height the height of the window.
+     *  A functional interface of the mouse callback.
+     *  @see SimpleGraphics#waitForMouseEvents(MouseCallback)
      */
-    public SimpleGraphics(String title, int width, int height) {
-        // This should be a factory method and not a constructor
-        // but given that this library can be used
-        // before static factory have been introduced
-        // it's simpler from the user point of view
-        // to create a canvas area using new.
-
-        BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        @SuppressWarnings("serial")
-        JComponent area = new class MouseManager extends MouseAdapter implements MouseMotionListener {
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                try {
-                    eventBlockingQueue.put(event);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                // do nothing
-            }
-
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                mouseClicked(e);
-            }
-        }
-        ;
-        JComponent() {
-            @Override
-            protected void paintComponent (Graphics g){
-                g.drawImage(buffer, 0, 0, null);
-            }
-
-            @Override
-            public Dimension getPreferredSize () {
-                return new Dimension(width, height);
-            }
-        }
-        MouseManager mouseManager = new MouseManager();
-        area.addMouseListener(mouseManager);
-        area.addMouseMotionListener(mouseManager);
-        try {
-            EventQueue.invokeAndWait(() -> {
-                JFrame frame = new JFrame(title);
-                area.setOpaque(true);
-                frame.setContentPane(area);
-                frame.setResizable(false);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-            });
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } catch (InvocationTargetException e) {
-            throw new IllegalStateException(e.getCause());
-        }
-        this.buffer = buffer;
-        this.area = area;
+    @FunctionalInterface
+    public interface MouseCallback {
+        /**
+         * Called when the mouse is used inside the canvas.
+         * @param x x coordinate of the mouse.
+         * @param y y coordinate of the mouse
+         *
+         * @see SimpleGraphics#waitForMouseEvents(MouseCallback)
+         */
+        void mouseClicked(int x, int y);
     }
 
     /**
@@ -141,7 +153,7 @@ public class SimpleGraphics {
         if (EventQueue.isDispatchThread()) {
             throw new IllegalStateException("This method can not be called from the EDT");
         }
-        for (; ; ) {
+        for(;;) {
             MouseEvent mouseEvent;
             try {
                 mouseEvent = eventBlockingQueue.take();
@@ -156,32 +168,19 @@ public class SimpleGraphics {
         }
     }
 
+    private static final MouseEvent CLOSE_EVENT =
+            new MouseEvent(new JButton(), -1, -1, -1, 0, 0, 0, 0, 0, false, 0);
+
     /**
      * Close the window.
      */
     public void close() {
-        JFrame frame = (JFrame) SwingUtilities.getRoot(area);
+        JFrame frame = (JFrame)SwingUtilities.getRoot(area);
         frame.dispose();
         try {
             eventBlockingQueue.put(CLOSE_EVENT);
-        } catch (InterruptedException e) {
+        } catch(InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-    }
-
-    /**
-     *  A functional interface of the mouse callback.
-     *  @see SimpleGraphics#waitForMouseEvents(MouseCallback)
-     */
-    @FunctionalInterface
-    public interface MouseCallback {
-        /**
-         * Called when the mouse is used inside the canvas.
-         * @param x x coordinate of the mouse.
-         * @param y y coordinate of the mouse
-         *
-         * @see SimpleGraphics#waitForMouseEvents(MouseCallback)
-         */
-        void mouseClicked(int x, int y);
     }
 }
