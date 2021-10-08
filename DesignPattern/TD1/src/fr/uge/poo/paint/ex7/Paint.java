bbfc15a52006974f57c2601c5fa3834e63827e1b
notKamui <teillard.jimmy@outlook.fr>
2021-10-08 15:16:56 +0200

    Update

--- DesignPattern/TD1/src/fr/uge/poo/paint/ex7/Paint.java
+++ DesignPattern/TD1/src/fr/uge/poo/paint/ex7/Paint.java
@@ -1,9 +1,13 @@
 package fr.uge.poo.paint.ex7;
 
+import java.util.Arrays;
+
 public final class Paint {
     public static void main(String[] args) {
-        var sm = ShapeManager.from("resources/draw-big.txt");
-        var canvas = new Canvas(sm);
-        canvas.open();
+        if (Arrays.asList(args).contains("-legacy")) {
+            var sm = ShapeManager.from("resources/draw2.txt");
+            var canvas = new Canvas(sm);
+            canvas.open();
+        }
     }
 }
