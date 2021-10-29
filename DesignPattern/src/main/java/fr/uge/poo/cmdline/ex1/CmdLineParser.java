71e0eb6aac21e6b7aaff4a167494f3f1a04e2cf4
notKamui <teillard.jimmy@outlook.fr>
2021-10-29 15:12:15 +0200

    Update

--- DesignPattern/src/main/java/fr/uge/poo/cmdline/ex1/CmdLineParser.java
+++ DesignPattern/src/main/java/fr/uge/poo/cmdline/ex1/CmdLineParser.java
@@ -6,25 +6,40 @@ import java.util.List;
 import java.util.Map;
 import static java.util.Objects.requireNonNull;
 
-public class CmdLineParser {
+/**
+ * A simple command line parser/processor.
+ */
+public final class CmdLineParser {
 
     private final Map<String, Runnable> optionToProcess = new HashMap<>();
 
+    /**
+     * Registers an option along with its linked process.
+     *
+     * @param option the name of the option
+     * @param process the runnable process linked to the option
+     */
     public void registerOption(String option, Runnable process) {
         requireNonNull(option);
         requireNonNull(process);
         optionToProcess.put(option, process);
     }
 
+    /**
+     * Processes an array of arguments.
+     *
+     * @param arguments an array of arguments to process
+     * @return the list of arguments that are not registered options
+     */
     public List<String> process(String[] arguments) {
-        ArrayList<String> files = new ArrayList<>();
-        for (String argument : arguments) {
+        var unregistered = new ArrayList<String>();
+        for (var argument : arguments) {
             if (optionToProcess.containsKey(argument)) {
                 optionToProcess.get(argument).run();
             } else {
-                files.add(argument);
+                unregistered.add(argument);
             }
         }
-        return files;
+        return unregistered;
     }
 }
--- DesignPattern/src/test/java/fr/uge/poo/cmdline/ex1/CmdLineParserTest.java
+++ DesignPattern/src/test/java/fr/uge/poo/cmdline/ex1/CmdLineParserTest.java
@@ -1,7 +1,12 @@
 package fr.uge.poo.cmdline.ex1;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CmdLineParserTest {
 
@@ -11,4 +16,50 @@ class CmdLineParserTest {
         assertThrows(NullPointerException.class, () -> parser.process(null));
     }
 
+    @Test
+    public void processOnlyUnregistered() {
+        var parser = new CmdLineParser();
+        String[] args = { "aaa", "bbb", "ccc" };
+        assertTrue(parser.process(args).containsAll(List.of("aaa", "bbb", "ccc")));
+    }
+
+    @Test
+    public void processOnlyFlags() {
+        var parser = new CmdLineParser();
+        var flags = new HashMap<>(Map.of(
+                "a", false,
+                "b", false,
+                "c", false
+        ));
+        parser.registerOption("-a", () -> flags.put("a", true));
+        parser.registerOption("-b", () -> flags.put("b", true));
+        parser.registerOption("-c", () -> flags.put("c", true));
+
+        String[] args0 = { "-b", "-c" };
+        parser.process(args0);
+        assertFalse(flags.get("a"));
+        assertTrue(flags.get("b"));
+        assertTrue(flags.get("c"));
+    }
+
+    @Test
+    public void processAllOptions() {
+        var parser = new CmdLineParser();
+        var flags = new HashMap<>(Map.of(
+                "a", false,
+                "b", false,
+                "c", false
+        ));
+
+        parser.registerOption("-a", () -> flags.put("a", true));
+        parser.registerOption("-b", () -> flags.put("b", true));
+        parser.registerOption("-c", () -> flags.put("c", true));
+
+        String[] args0 = { "-b", "aaa", "-c", "bbb" };
+        var unregistered = parser.process(args0);
+        assertFalse(flags.get("a"));
+        assertTrue(flags.get("b"));
+        assertTrue(flags.get("c"));
+        assertTrue(unregistered.containsAll(List.of("aaa", "bbb")));
+    }
 }
