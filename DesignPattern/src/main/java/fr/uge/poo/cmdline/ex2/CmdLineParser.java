4b134d707f56486cca728d370256d22fefe20828
notKamui <teillard.jimmy@outlook.fr>
2021-10-29 15:19:17 +0200

    Update

---
+++ DesignPattern/src/main/java/fr/uge/poo/cmdline/ex2/CmdLineParser.java
@@ -0,0 +1,45 @@
+package fr.uge.poo.cmdline.ex2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A simple command line parser/processor.
+ */
+public final class CmdLineParser {
+
+    private final Map<String, Runnable> optionToProcess = new HashMap<>();
+
+    /**
+     * Registers an option along with its linked process.
+     *
+     * @param option the name of the option
+     * @param process the runnable process linked to the option
+     */
+    public void registerOption(String option, Runnable process) {
+        requireNonNull(option);
+        requireNonNull(process);
+        optionToProcess.put(option, process);
+    }
+
+    /**
+     * Processes an array of arguments.
+     *
+     * @param arguments an array of arguments to process
+     * @return the list of arguments that are not registered options
+     */
+    public List<String> process(String[] arguments) {
+        var unregistered = new ArrayList<String>();
+        for (var argument : arguments) {
+            if (optionToProcess.containsKey(argument)) {
+                optionToProcess.get(argument).run();
+            } else {
+                unregistered.add(argument);
+            }
+        }
+        return unregistered;
+    }
+}
---
+++ DesignPattern/src/test/java/fr/uge/poo/cmdline/ex2/CmdLineParserTest.java
@@ -0,0 +1,73 @@
+package fr.uge.poo.cmdline.ex2;
+
+import fr.uge.poo.cmdline.ex1.CmdLineParser;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CmdLineParserTest {
+
+    @Test
+    public void invalidOptionRegistration() {
+        var parser = new fr.uge.poo.cmdline.ex1.CmdLineParser();
+        assertThrows(NullPointerException.class, () -> parser.registerOption(null, () -> {}));
+        assertThrows(NullPointerException.class, () -> parser.registerOption("", null));
+    }
+
+    @Test
+    public void processShouldFailFastOnNullArgument(){
+        var parser = new fr.uge.poo.cmdline.ex1.CmdLineParser();
+        assertThrows(NullPointerException.class, () -> parser.process(null));
+    }
+
+    @Test
+    public void processOnlyUnregistered() {
+        var parser = new fr.uge.poo.cmdline.ex1.CmdLineParser();
+        String[] args = { "aaa", "bbb", "ccc" };
+        assertTrue(parser.process(args).containsAll(List.of("aaa", "bbb", "ccc")));
+    }
+
+    @Test
+    public void processOnlyFlags() {
+        var parser = new fr.uge.poo.cmdline.ex1.CmdLineParser();
+        var flags = new HashMap<>(Map.of(
+                "a", false,
+                "b", false,
+                "c", false
+        ));
+        parser.registerOption("-a", () -> flags.put("a", true));
+        parser.registerOption("-b", () -> flags.put("b", true));
+        parser.registerOption("-c", () -> flags.put("c", true));
+
+        String[] args0 = { "-b", "-c" };
+        parser.process(args0);
+        assertFalse(flags.get("a"));
+        assertTrue(flags.get("b"));
+        assertTrue(flags.get("c"));
+    }
+
+    @Test
+    public void processAllOptions() {
+        var parser = new CmdLineParser();
+        var flags = new HashMap<>(Map.of(
+                "a", false,
+                "b", false,
+                "c", false
+        ));
+
+        parser.registerOption("-a", () -> flags.put("a", true));
+        parser.registerOption("-b", () -> flags.put("b", true));
+        parser.registerOption("-c", () -> flags.put("c", true));
+
+        String[] args0 = { "-b", "aaa", "-c", "bbb" };
+        var unregistered = parser.process(args0);
+        assertFalse(flags.get("a"));
+        assertTrue(flags.get("b"));
+        assertTrue(flags.get("c"));
+        assertTrue(unregistered.containsAll(List.of("aaa", "bbb")));
+    }
+}
