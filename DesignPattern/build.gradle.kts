118a17e2afe03cb379a88b6db2f0269dd31aa4e0
notKamui <teillard.jimmy@outlook.fr>
2021-10-29 14:11:14 +0200

    Update

---
+++ DesignPattern/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+}
+
+group = "fr.uge.poo"
+version = "1.0-SNAPSHOT"
+val jvmVersion = "17"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = jvmVersion
+    targetCompatibility = jvmVersion
+    options.compilerArgs.add("--enable-preview")
+    options.compilerArgs.add("-Xlint:preview")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
