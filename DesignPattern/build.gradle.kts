fa3c0d70c13b49649cc4bedf0787eec59cf1b8be
notKamui <teillard.jimmy@outlook.fr>
2021-12-02 12:39:28 +0100

    Update

--- DesignPattern/build.gradle.kts
+++ DesignPattern/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
+    application
 }
 
 group = "fr.uge.poo"
 version = "1.0-SNAPSHOT"
-val jvmVersion = "17"
 
 repositories {
     mavenCentral()
@@ -15,14 +15,20 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks.withType<JavaCompile> {
-    sourceCompatibility = jvmVersion
-    targetCompatibility = jvmVersion
     options.compilerArgs.add("--enable-preview")
-    options.compilerArgs.add("-Xlint:all")
 }
 
 tasks.test {
     useJUnitPlatform()
     jvmArgs("--enable-preview")
+}
+
+application {
+    mainClass.set("fr.uge.poo.visitors.stp.ApplicationHipster")
 }
