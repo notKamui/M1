b55651ad5df0215abece7773f9439039c57b2dc2
notKamui <teillard.jimmy@outlook.fr>
2021-12-09 12:09:32 +0100

    Update

--- DesignPattern/src/main/java/fr/uge/poo/visitors/expr/Application.java
+++ DesignPattern/src/main/java/fr/uge/poo/visitors/expr/Application.java
@@ -6,11 +6,13 @@ public class Application {
     public static void main(String[] args) {
         var it = Pattern.compile(" ").splitAsStream("+ * 4 + 1 1 + 2 3").iterator();
         var expr = Expr.parseExpr(it);
+
         var evaluator = new EvalExprVisitor();
+        System.out.println(expr.accept(evaluator, null));
+
         var toString = new ToStringVisitor();
         var sb = new StringBuilder();
         expr.accept(toString, sb);
         System.out.println(sb);
-        System.out.println(expr.accept(evaluator, null));
     }
 }
