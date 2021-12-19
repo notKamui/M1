8109b4e51be1f39e6440e762fb8a023f5f582e59
notKamui <teillard.jimmy@outlook.fr>
2021-12-19 10:32:31 +0100

    Update

--- Opti/TP-minisat/sudoku.py
+++ Opti/TP-minisat/sudoku.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
 """Sudoku"""
 
 import itertools
@@ -7,20 +6,23 @@ import math
 import sys
 import doctest
 
-def var(i,j,k):
+
+def var(i, j, k):
     """Return the literal Xijk.
     """
-    return (1,i,j,k)
+    return (1, i, j, k)
+
 
 def neg(l):
     """Return the negation of the literal l.
     """
-    (s,i,j,k) = l
-    return (-s,i,j,k)
+    (s, i, j, k) = l
+    return (-s, i, j, k)
+
 
 def initial_configuration():
     """Return the initial configuration of the example in td6.pdf
-    
+
     >>> cnf = initial_configuration()
     >>> [(1, 1, 4, 4)] in cnf
     True
@@ -31,10 +33,11 @@ def initial_configuration():
     """
     return [[var(1, 4, 4)], [var(2, 1, 2)], [var(3, 2, 1)], [var(4, 3, 1)]]
 
+
 def at_least_one(L):
     """Return a cnf that represents the constraint: at least one of the
     literals in the list L is true.
-    
+
     >>> lst = [var(1, 1, 1), var(2, 2, 2), var(3, 3, 3)]
     >>> cnf = at_least_one(lst)
     >>> len(cnf)
@@ -48,10 +51,11 @@ def at_least_one(L):
     """
     return [L]
 
+
 def at_most_one(L):
     """Return a cnf that represents the constraint: at most one of the
     literals in the list L is true
-    
+
     >>> lst = [var(1, 1, 1), var(2, 2, 2), var(3, 3, 3)]
     >>> cnf = at_most_one(lst)
     >>> len(cnf)
@@ -73,16 +77,18 @@ def at_most_one(L):
         ret.append(clause)
     return ret
 
+
 def assignment_rules(N):
     """Return a list of clauses describing the rules for the assignment (i,j) -> k.
     """
     cnf = []
-    for i in range(1,N+1):
-        for j in range(1,N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
             node = [var(i, j, k) for k in range(1, N+1)]
             cnf += at_least_one(node)
             cnf += at_most_one(node)
     return cnf
+
 
 def row_rules(N):
     """Return a list of clauses describing the rules for the rows.
@@ -95,6 +101,7 @@ def row_rules(N):
             cnf += at_most_one(line)
     return cnf
 
+
 def column_rules(N):
     """Return a list of clauses describing the rules for the columns.
     """
@@ -105,6 +112,7 @@ def column_rules(N):
             cnf += at_least_one(col)
             cnf += at_most_one(col)
     return cnf
+
 
 def subgrid_rules(N):
     """Return a list of clauses describing the rules for the subgrids.
@@ -122,15 +130,17 @@ def subgrid_rules(N):
                 cnf += at_most_one(sub)
     return cnf
 
+
 def generate_rules(N):
     """Return a list of clauses describing the rules of the game.
     """
-    cnf = []    
+    cnf = []
     cnf.extend(assignment_rules(N))
     cnf.extend(row_rules(N))
     cnf.extend(column_rules(N))
     cnf.extend(subgrid_rules(N))
     return cnf
+
 
 def literal_to_integer(l, N):
     """Return the external representation of the literal l.
@@ -143,22 +153,40 @@ def literal_to_integer(l, N):
     (s, i, j, k) = l
     return s*(N*N*(i - 1) + N*(j - 1) + k)
 
+
 def writeCNFToFile(fname, cnf, N):
     """Writes the CNF of a board of size N to a given file
     """
     with open(fname, "w+") as f:
         f.write("p cnf %d %d\n" % (N**3, len(cnf)))
         for clause in cnf:
-            line = ' '.join(map(lambda term: str(literal_to_integer(term, N)), clause))
+            line = ' '.join(map(lambda term: str(
+                literal_to_integer(term, N)), clause))
             f.write("%s 0\n" % line)
+
+
+def parseCNFFromFile(fname):
+    """Reads a CNF from a file and returns a list of clauses and the dimension of the grid.
+    """
+    with open(fname, "r") as f:
+        lines = f.readlines()
+        N = int(lines[0])
+        grid = list(map(lambda line: list(
+            map(lambda term: int(term), line.split())
+        ), lines[1:]))
+        cnf = [[var(i+1, j+1, grid[i][j])]
+               for i in range(N)
+               for j in range(N)
+               if grid[i][j] != 0]
+    return cnf, N
 
 
 def main():
     doctest.testmod()
 
-    fname = sys.argv[1]
-    cnf = initial_configuration() + generate_rules(4)
-    writeCNFToFile(fname, cnf, 4)
+    cnf, N = parseCNFFromFile(sys.argv[1])
+    writeCNFToFile(sys.argv[1] + ".cnf", cnf + generate_rules(N), N)
+
 
 if __name__ == "__main__":
     main()
