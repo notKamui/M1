fb617fdcbc2520adf9b3000967e6a0e98d3ddffc
notKamui <teillard.jimmy@outlook.fr>
2022-01-14 12:26:39 +0100

    Update

---
+++ Reseau/TP1/src/fr/upem/net/buffers/ReadStandardInputWithEncoding.java
@@ -0,0 +1,40 @@
+package fr.upem.net.buffers;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.charset.Charset;
+
+public class ReadStandardInputWithEncoding {
+
+	private static final int BUFFER_SIZE = 1024;
+
+	private static void usage() {
+		System.out.println("Usage: ReadStandardInputWithEncoding charset");
+	}
+
+	private static String stringFromStandardInput(Charset cs) throws IOException {
+		try (var channel = Channels.newChannel(System.in)) {
+			var bb = ByteBuffer.allocate(64);
+			while (channel.read(bb) != -1) {
+				if (!bb.hasRemaining()) {
+					var newbb = ByteBuffer.allocate(bb.capacity() * 2);
+					bb.flip();
+					newbb.put(bb);
+					bb = newbb;
+				}
+			}
+			bb.flip();
+			return cs.decode(bb).toString();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			usage();
+			return;
+		}
+		Charset cs = Charset.forName(args[0]);
+		System.out.print(stringFromStandardInput(cs));
+	}
+}
--- Reseau/TP1/src/fr/upem/net/buffers/StoreWithByteOrder.java
+++ Reseau/TP1/src/fr/upem/net/buffers/StoreWithByteOrder.java
@@ -1,11 +1,14 @@
 package fr.upem.net.buffers;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.util.Scanner;
-
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 public class StoreWithByteOrder {
 
@@ -20,26 +23,29 @@ public class StoreWithByteOrder {
 		}
 		var path = Path.of(args[1]);
 
-		// TODO
+		var order = switch (args[0].toUpperCase()) {
+			case "LE" -> ByteOrder.LITTLE_ENDIAN;
+			case "BE" -> ByteOrder.BIG_ENDIAN;
+			default -> {
+				System.out.println("Unrecognized option : " + args[0]);
+				usage();
+				yield null;
+			}
+		};
+		if (order == null) return;
 
-		switch (args[0].toUpperCase()) {
-		case "LE":
-			// TODO
-			break;
-		case "BE":
-			// TODO
-			break;
-		default:
-			System.out.println("Unrecognized option : " + args[0]);
-			usage();
-			return;
-		}
-
-		try (var outChannel = FileChannel.open(path, WRITE, CREATE, TRUNCATE_EXISTING);
-				var scanner = new Scanner(System.in)) {
+		var bb = ByteBuffer.allocate(8);
+		bb.order(order);
+		try (
+			var outChannel = FileChannel.open(path, WRITE, CREATE, TRUNCATE_EXISTING);
+			var scanner = new Scanner(System.in)
+		) {
 			while (scanner.hasNextLong()) {
 				var l = scanner.nextLong();
-				// TODO
+				bb.putLong(l);
+				bb.flip();
+				outChannel.write(bb);
+				bb.clear();
 			}
 		}
 	}
